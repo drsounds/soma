347aa1d0a35303df3e7f347ad82f62f67894abb9
Alexander Forselius <drsounds@gmail.com>
2022-07-09 12:01:03 +0200

    Improved building

--- assets/Soma/components/house/Bungalow.gd
+++ assets/Soma/components/house/Bungalow.gd
@@ -5,22 +5,8 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
-export (String) var entrance_code setget set_entrance_code, get_entrance_code
-
-export (bool) var is_entrance_locked setget set_is_entrance_locked, get_is_entrance_locked
-
-func get_entrance_code():
-	return get_node("EntranceDoor").code
-	
-func set_entrance_code(value):
-	get_node("EntranceDoor").code = value 
-
-func get_is_entrance_locked():
-	return get_node("EntranceDoor").is_locked
-	
-func set_is_entrance_locked(value):
-	get_node("EntranceDoor").is_locked = value 
-
+export (String) var entrance_code
+export (bool) var is_entrance_locked
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
